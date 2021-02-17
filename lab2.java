19cd86e6098b2699f42616f95f45fbbef23ce661
Sofia Esparza-Chavez <sesparzachavez2019@aolp.org>
2021-02-16 23:23:48 -0800

    final commit

--- lab2.java
+++ lab2.java
@@ -3,13 +3,20 @@ import java.util.Arrays;
 public class lab2 {
 
     public int [] twoSum(int[] arr, int targetNum){
+        //creating array that will contain the two indexes of the integers that add up to the target
         int [] sumArr = new int [2];
-        for(int i = 0; i < arr.length-1;i++){
-            for(int j = i+1; j<arr.length;j++){
-                if((arr[i]+arr[j]) == targetNum)
-                    sumArr[0]= i;
-                    sumArr[1]= j;
 
+        //the outer loop is in charge of checking the first of two indexes of the result
+        //it goes until length-1 because it does not need to check the last, the nested loop will
+        for(int i = 0; i < arr.length-1;i++){
+            //the nested loop is in charge of the second of the two indexes of the result
+            //this is why it starts with i + 1
+            for(int j = i+1; j<arr.length;j++){
+                if((arr[i]+arr[j]) == targetNum) //checks whether the two elements of the arrays add up to the target
+
+                    //stores the indexes that add up to target
+                    sumArr[0]= i;
+                sumArr[1]= j;
             }
 
         }
